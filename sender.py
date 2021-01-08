fc4a8cc5dbfcfb1cb47a5ec1d6486530cc467d7a
kalinkinn <75300023+kalinkinn@users.noreply.github.com>
2021-01-08 13:09:31 +0600

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,11 @@
+import requests
+
+name = input('Введите имя: ')
+
+while True:
+    text = input()
+    requests.post('http://127.0.0.1:5000/send', json=
+    {
+        'name': name,
+        'text': text
+    })
